5721fc317324dc21fb8aea3b9c5eb758949de605
juniorHotes <juniorhotes53@gmail.com>
2021-02-11 14:04:31 -0300

    Now the page is written by data.json.

--- public/js/write.js
+++ public/js/write.js
@@ -1,57 +1,55 @@
-const $ = document.querySelector.bind(document)
-
-fetch('README.md')
-    .then(res => res.text())
+fetch('public/data.json')
+    .then(res => res.json())
     .then(file => {
-        const data = file.split('#')
-
-        $('.about-me h1').innerHTML = data[2].split('\n')[0]
-        const About = data[2].split('\n')[1]
-        const keys = About.split('')
+        const keys = file.about_me.split('')
 
         let i = 0
         const _frame = setInterval(frame, 10);
         function frame() {
             if (i < keys.length) {
-                $('#txt-about').innerHTML += keys[i]
+                document.querySelector('#txt-about').innerHTML += keys[i]
                 i++
             } else if (i == keys.length) {
                 clearInterval(_frame)
             }
         }
 
-        $('.technologies h1').innerHTML = data[3].split('\n')[0]
-        const TechnologiesMaster = data[3].split('Key-skills')[1].split('\n')
-        TechnologiesMaster.map(item => {
-            return item.search('<img') != -1 ? $('.technologies ul').innerHTML += "<li>" + item + "</li>" : null
+        file.skills.map(skll => {
+            document.querySelector('.technologies ul').innerHTML += 
+            `<li><img src="${skll.src}" alt="${skll.title}" title="${skll.title}"></img></li>`
         })
 
-        const projects = data[4].split('<h3>')
-        projects.forEach((item, idx) => {
-            if (idx == 0) return
-
-            const title = item.split('</h3>')[0]
-            const desc = item.split('</h3>')[1].split('alt="">')[1].split('*')[0].trim()
-            const img = item.split('</h3>')[1].split('\n')[1]
-            const tecnology = item.split('</h3>')[1].split('alt="">')[1].split('*')[2].trim()
-
+        file.projects.map(project => {
             const containerProject = document.createElement('div')
             containerProject.className = 'container-project'
 
-            containerProject.innerHTML = `
-                <div class="project-container-img">
-                    <h1 class="project-title">${title}</h1>
-                    ${img}
-                </div>
-                <div class="container-info">
-                    <div></div>
-                    <p class="project-description">${desc}</p>
-                    <div class="project-footer-tecnologies">
-                        <ul>${tecnology}</ul>
-                    </div>
-                </div>`
+            const carouselImages = project.carousel_img.map(url => `<div><img style="object-fit: scale-down; width:100%" src="${url}" alt=""></div>`)
 
-            $('.container').appendChild(containerProject)
+            const tecnologies = project.tecnologies.map(tec =>
+                `<li><img src="${tec.src}" alt="${tec.title}" title="${tec.title}"></li>`
+            )
+
+
+            containerProject.innerHTML = `
+                    <div class="container-info">
+                        <div>
+                            <h1 class="project-title">${project.name}</h1>
+                            <a class="repo-git" href="${project.link_git}" target="_blank" rel="">Repositório</a>
+                        </div>
+                        <p class="project-description">${project.desc}</p>
+                        <div class="project-footer-tecnologies">
+                            <h2>Tecnologias utilizadas</h2>
+                            <ul>${tecnologies.join('')}</ul>
+                        </div>
+                    </div>
+                    <div class="project-container-img">
+                    <div class="container-carousel">
+                        <div class="carousel">${carouselImages.join('')}</div>
+                    </div>
+                </div>
+`
+
+            document.querySelector('.container').appendChild(containerProject)
         })
 
-    });
+    })
