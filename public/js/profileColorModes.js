9474e2a599ce3fde14ec3c5626c520b694e778f0
juniorHotes <juniorhotes53@gmail.com>
2021-02-11 14:01:10 -0300

    Refactoring.

--- public/js/profileColorModes.js
+++ public/js/profileColorModes.js
@@ -1,20 +1,20 @@
 const profileColor = localStorage.getItem('profile-color-modes')
 
 if (profileColor === 'checked') {
-    $('#theme').setAttribute('href', "public/styles/lightmode.css")
-    $('.profile-color-modes-toggle-thumb').classList.add('checked')
+    document.querySelector('#theme').setAttribute('href', "public/styles/lightmode.css")
+    document.querySelector('.profile-color-modes-toggle-thumb').classList.add('checked')
 }
 
-$('.profile-color-modes-toggle-thumb').addEventListener('click', (e) => {
-    const isChecked = $('.profile-color-modes-toggle-thumb').getAttribute('class').split(' ')[1]
+document.querySelector('.profile-color-modes-toggle-thumb').addEventListener('click', (e) => {
+    const isChecked = document.querySelector('.profile-color-modes-toggle-thumb').getAttribute('class').split(' ')[1]
 
     if (isChecked === 'checked') {
-        $('#theme').setAttribute('href', "public/styles/darkmode.css")
-        $('.profile-color-modes-toggle-thumb').classList.remove('checked')
+        document.querySelector('#theme').setAttribute('href', "public/styles/darkmode.css")
+        document.querySelector('.profile-color-modes-toggle-thumb').classList.remove('checked')
         localStorage.removeItem('profile-color-modes')
     } else {
-        $('#theme').setAttribute('href', "public/styles/lightmode.css")
-        $('.profile-color-modes-toggle-thumb').classList.add('checked')
+        document.querySelector('#theme').setAttribute('href', "public/styles/lightmode.css")
+        document.querySelector('.profile-color-modes-toggle-thumb').classList.add('checked')
         localStorage.setItem('profile-color-modes', 'checked')
     }
 });
