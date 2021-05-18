94bc3c41c4eda33bb6f79cbf9d782c67bdefe4a1
juniorHotes <juniorhotes53@gmail.com>
2021-05-18 18:03:18 -0300

    Adding react version.

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import './styles/styles.css'
+import Home from './pages/Home'
+
+function App(): React.ReactElement {
+  return <Home />
+}
+
+export default App;
